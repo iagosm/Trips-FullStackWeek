51936c1c41b59df7fee65acaa86e7c61e1b2109a
iagosm <iagosousa201486@gmail.com>
2023-07-03 22:56:10 -0300

    iniciando projeto da semana Full Stack Week

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const metadata = { // utilizado para dar um title a page
+  title: "About",
+};
+
+const page = () => {
+  return  <div>About</div> ;
+};
+
+export default page
---
+++ src/app/about/trips/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return  (
+    <div>a World</div>
+  )
+}
+
+export default page
