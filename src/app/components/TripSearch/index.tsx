07bd8224c918b2b37ac5e0a5acb4a15399001f2b
iagosm <iagosousa201486@gmail.com>
2023-07-06 22:06:07 -0300

    feat: add input datepicker and currency input components

--- src/app/components/TripSearch/index.tsx
+++ src/app/components/TripSearch/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import DatePicker from '@/components/DatePicker';
+import CurrencyInput from '@/components/CurrencyInput';
+import Input from '@/components/Input';
+import React from 'react';
 
 const TripSearch = () => {
   return (
     <div className='container mx-auto p-5'>
         <h1 className='font-semibold text-2xl text-primaryDarker text-center'>Encontre sua próxima <span className='text-primary'>viagem!</span></h1>
+        <div className="flex flex-col gap-4 mt-5">
+        <Input placeholder='Onde você quer ir?'/>
+        <div className="flex gap-4">
+          <DatePicker placeholderText='Data de Ida' onChange={() => {}} className='w-full'/>
+          <CurrencyInput placeholder='Orçamento'/>
+        </div>
+        </div>
+    
     </div>
   )
 }
