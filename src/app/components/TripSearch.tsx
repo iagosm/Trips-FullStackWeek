6c836ae1c256f552652c8053da8de2afd0c581bd
iagosm <iagosousa201486@gmail.com>
2023-07-06 22:21:40 -0300

    feat: add inital search trip

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,11 +1,12 @@
 import DatePicker from '@/components/DatePicker';
 import CurrencyInput from '@/components/CurrencyInput';
 import Input from '@/components/Input';
+import Button from '@/components/Button';
 import React from 'react';
 
 const TripSearch = () => {
   return (
-    <div className='container mx-auto p-5'>
+    <div className='container mx-auto p-5 bg-search-background bg-cover bbg-center bg-no-repeat'>
         <h1 className='font-semibold text-2xl text-primaryDarker text-center'>Encontre sua próxima <span className='text-primary'>viagem!</span></h1>
         <div className="flex flex-col gap-4 mt-5">
         <Input placeholder='Onde você quer ir?'/>
@@ -13,8 +14,8 @@ const TripSearch = () => {
           <DatePicker placeholderText='Data de Ida' onChange={() => {}} className='w-full'/>
           <CurrencyInput placeholder='Orçamento'/>
         </div>
+        <Button>Buscar</Button>
         </div>
-    
     </div>
   )
 }
