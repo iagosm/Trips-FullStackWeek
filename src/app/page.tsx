9085ef79d874bf5938a5b8186e3c77014e088ee0
iagosm <iagosousa201486@gmail.com>
2023-07-09 17:41:06 -0300

    feat: add quick search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import QuickSearch from "./components/QuickSearch";
 import TripSearch from "./components/TripSearch";
 
 export default function Home() {
   return ( 
   <div >
     <TripSearch />
+    <QuickSearch/>
     </div>
 )}
