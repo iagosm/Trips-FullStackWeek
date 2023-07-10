a604ecb0610f4207cf919583a0b86e93f87f80c0
iagosm <iagosousa201486@gmail.com>
2023-07-09 23:07:09 -0300

    feat: add trip item componente and add recommended trips

--- src/app/components/RecommendedTrips.tsx
+++ src/app/components/RecommendedTrips.tsx
@@ -1,7 +1,10 @@
- import React from 'react';
+import TripItem from '@/components/TripItem';
+import { Trip } from '@prisma/client';
+import React from 'react';
 
- const RecommendedTrips = async () => {
+const RecommendedTrips = async () => {
   const data = await fetch('http://localhost:3000/hello').then(res => res.json())
+
   return (
     <div className='container mx-auto p-5'>
       <div className="flex items-center">
@@ -10,6 +13,11 @@
         <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
      
+     <div className="flex flex-col items-center mt-5 gap-5">
+      {data.map((trip: Trip) => (
+        <TripItem key={trip.id} trip={trip}/>
+      ))}
+     </div>
     </div>
   )
  }
